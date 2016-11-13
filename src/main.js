7f52ce686df3729b303ff4103d0b986d12500dda
davidsauntson <davidsauntson@email.com>
2016-11-13 15:07:30 +0000

    added routing with react-router

--- src/main.js
+++ src/main.js
@@ -1,42 +1,8 @@
 $ = jQuery = require('jquery');
 var React = require('react');
-var Home = require('./components/homePage');
-var About = require('./components/About/aboutPage');
-var Authors = require('./components/Authors/authorsPage');
-var Header = require('./components/common/header');
+var Router = require('react-router');
+var routes = require('./routes.js');
 
-var App = React.createClass({
-    render: function() {
-        var Child;
-
-        console.log(this.props.route);
-        switch(this.props.route) {
-            case 'about':
-                Child = About;
-                break;
-
-            case 'authors':
-                Child = Authors;
-                break;
-            default:
-                Child = Home;
-        };
-
-        return (
-            <div>
-                <Header/>
-                <Child/>
-            </div>
-        );
-    }
-});
-
-render = function() {
-    var route = window.location.hash.substr(1);
-    React.render(<App route={route} />, document.getElementById('app'));
-}
-
-
-window.addEventListener('hashchange', render);
-render();
-
+Router.run(routes, function(Handler) {
+    React.render(<Handler/>, document.getElementById('app'));
+})
